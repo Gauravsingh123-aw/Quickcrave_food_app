f730a6267639491be52fcdd618bdea0e39ff2dbb
Gaurav Singh <gauravsinghaw123@gmail.com>
2024-05-09 11:56:43 +0530

    adding dist to backend

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app= exp()
 
 const path=require('path') //for connecting the frontend
 
-app.use(exp.static(path.join(__dirname,'../project/dist')))
+app.use(exp.static(path.join(__dirname,'/dist')))
 // app.use(cors())
 
 
